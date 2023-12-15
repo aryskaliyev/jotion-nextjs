0eec857b5fcd2d28a88616a8eb49f26256662034
aryskaliyev <aryskaliyev>
2023-12-15 18:17:29 +0600

    paused on 5:19:40

--- notion-clone/app/(main)/_components/title.tsx
+++ notion-clone/app/(main)/_components/title.tsx
@@ -2,16 +2,43 @@
 
 import { useMutation } from "convex/react";
 
+import { Doc } from "@/convex/_generated/dataModel";
+import { api } from "@/convex/_generated/api";
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
 interface TitleProps {
-    initialData: Doc<"documents">;
+    initialData: Doc<"documents">,
 };
 
 export const Title = ({
-    initialData: Doc<"documents">;
+    initialData
 }: TitleProps) => {
+    const inputRef = useRef
+    const update = useMutation(api.documents.update);
+
+    const [isEditing, setIsEditing] = useState(false);
+
     return (
-        <div>
-            Title
+        <div className="flex items-center gap-x-1">
+            {!!initialData.icon && <p>{initialData.icon}</p>}
+            {isEditing ? (
+                <Input
+                    className="h-7 px-2 focus-visible:ring-transparent"
+                />
+            ) : (
+                <Button
+                    onClick={() => {}}
+                    variant="ghost"
+                    size="sm"
+                    className="font-normal h-auto p-1"
+                >
+                    <span className="truncate">
+                        {initialData?.title}
+                    </span>
+                </Button>
+            )}
         </div>
-    );
-};
+    )
+}
