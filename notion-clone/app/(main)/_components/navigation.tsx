d83a54383cd320d5b9046856a6673ce7a0760ff5
aryskaliyev <aryskaliyev>
2023-12-15 17:32:56 +0600

    paused on 5:16:47

--- notion-clone/app/(main)/_components/navigation.tsx
+++ notion-clone/app/(main)/_components/navigation.tsx
@@ -27,6 +27,7 @@ import {
 import { TrashBox } from "./trash-box";
 import { useSearch } from "@/hooks/use-search";
 import { useSettings } from "@/hooks/use-settings";
+import { Navbar } from "./navbar";
 
 const Navigation = () => {
     const settings = useSettings();
@@ -192,9 +193,16 @@ const Navigation = () => {
                     isMobile && "left-0 w-full"
                 )}
             >
-                <nav className="bg-transparent px-3 py-2 w-full">
-                    {isCollapsed && <MenuIcon onClick={resetWidth} role="button" className="h-6 w-6 text-muted-foreground" />}
-                </nav>
+                {!!params.documentId ? (
+                    <Navbar
+                        isCollapsed={isCollapsed}
+                        onResetWidth={resetWidth}
+                    />
+                ) : (
+                    <nav className="bg-transparent px-3 py-2 w-full">
+                        {isCollapsed && <MenuIcon onClick={resetWidth} role="button" className="h-6 w-6 text-muted-foreground" />}
+                    </nav>  
+                )}
             </div>
         </>
     );
---
+++ notion-clone/app/(main)/_components/title.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useMutation } from "convex/react";
+
+interface TitleProps {
+    initialData: Doc<"documents">;
+};
+
+export const Title = ({
+    initialData: Doc<"documents">;
+}: TitleProps) => {
+    return (
+        <div>
+            Title
+        </div>
+    );
+};
